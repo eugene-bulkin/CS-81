6f2ba4516576f20480d525ccff9c5afa7e5022cf
Eugene Bulkin <ebulkin@caltech.edu>
2016-05-14 13:59:56 -0700

    Update database.py
    Handle URL stripping

--- database.py
+++ database.py
@@ -1,9 +1,20 @@
 import os
 import sqlite3
 import json
+import re
+
+url_regex = re.compile("https?:\/\/t\.co\/([^\b]+)")
+
+def strip_urls(tweet):
+    text = tweet['text']
+    for url in tweet['urls']:
+        text = text.replace(url['url'], '')
+    text = re.sub(url_regex, "", text)
+    tweet['text'] = text.strip()
+    return tweet
 
 def create_table(con):
-    con.execute("CREATE TABLE IF NOT EXISTS tweets (id PRIMARY KEY, username, text, hashtag_json, symbol_json, url_json);")
+    con.execute("CREATE TABLE IF NOT EXISTS tweets (id PRIMARY KEY, username, text UNIQUE, original_text, hashtag_json, symbol_json, url_json);")
 
 def save_tweets(con):
     json_files = [fn for fn in os.listdir("results") if fn.endswith(".json")]
@@ -17,7 +28,10 @@ def save_tweets(con):
         j = json.load(open("results/" + fn))
 
         for tweet in j:
-            all_tweets.append((tweet['id'], tweet['username'], tweet['text'], json.dumps(tweet['hashtags']),json.dumps(tweet['symbols']),json.dumps(tweet['urls'])))
+            old_text = tweet['text']
+            tweet = strip_urls(tweet)
+
+            all_tweets.append((tweet['id'], tweet['username'], tweet['text'], old_text, json.dumps(tweet['hashtags']),json.dumps(tweet['symbols']), json.dumps(tweet['urls'])))
             i += 1
             if i % 50 == 0:
                 print "Processed tweet #%d..." % i
@@ -25,7 +39,7 @@ def save_tweets(con):
 
     # ignore handles duplicates
     try:
-        cur.executemany("INSERT OR IGNORE INTO tweets(id, username, text, hashtag_json, symbol_json, url_json) VALUES (?, ?, ?, ?, ?, ?)", all_tweets)
+        cur.executemany("INSERT OR IGNORE INTO tweets(id, username, text, original_text, hashtag_json, symbol_json, url_json) VALUES (?, ?, ?, ?, ?, ?, ?)", all_tweets)
         print "Successfully saved %d unique tweets." % cur.rowcount
     except Exception as e:
         print "Unable to successfully save tweets: %s" % e
